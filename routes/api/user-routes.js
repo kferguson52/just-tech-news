160b19d437a23b957b7776879631a365af14e639
Kyle <kferguson@captechconsulting.com>
2021-11-02 18:21:49 -0500

    sync

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,6 +1,5 @@
 const router = require('express').Router();
-const { User } = require('../../models');
-const bcrypt = require('bcrypt');
+const { User, Post, Comment, Vote } = require('../../models');
 
 // get all users
 router.get('/', (req, res) => {
@@ -19,7 +18,27 @@ router.get('/:id', (req, res) => {
     attributes: { exclude: ['password'] },
     where: {
       id: req.params.id
-    }
+    },
+    include: [
+      {
+        model: Post,
+        attributes: ['id', 'title', 'post_url', 'created_at']
+      },
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'created_at'],
+        include: {
+          model: Post,
+          attributes: ['title']
+        }
+      },
+      {
+        model: Post,
+        attributes: ['title'],
+        through: Vote,
+        as: 'voted_posts'
+      }
+    ]
   })
     .then(dbUserData => {
       if (!dbUserData) {
@@ -67,7 +86,7 @@ router.post('/login', (req, res) => {
       return;
     }
 
-     res.json({ user: dbUserData, message: 'You are now logged in!' });
+    res.json({ user: dbUserData, message: 'You are now logged in!' });
   });
 });
 
