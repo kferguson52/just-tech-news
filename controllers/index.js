fb077d920aeb7f1b6fbe9d61ae30e146a828176a
Kyle <kferguson@captechconsulting.com>
2021-11-11 13:17:18 -0600

    fixed my models and routes

--- controllers/index.js
+++ controllers/index.js
@@ -2,11 +2,7 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 
-const homeRoutes = require('./api/home-routes');
-
 router.use('/api', apiRoutes);
-
-router.use('/', homeRoutes);
 
 router.use((req, res) => {
   res.status(404).end();
