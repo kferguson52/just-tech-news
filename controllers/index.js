1924929e95a6cfe00aafa362e74da31dd2ac78c0
Kyle <kferguson@captechconsulting.com>
2021-11-11 08:56:28 -0600

    debug this error (C:\Users\kferguson\OneDrive - CAPTECH VENTURES INC\Desktop\Full Stack Bootcamp\Week 14\Attemp 2 - Challenge\just-tech-news\server.js:8:1)

---
+++ controllers/index.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+
+const apiRoutes = require('./api');
+
+const homeRoutes = require('./api/home-routes');
+
+router.use('/api', apiRoutes);
+
+router.use('/', homeRoutes);
+
+router.use((req, res) => {
+  res.status(404).end();
+});
+
+module.exports = router;
